06981e34c6415b9e88cd4edc8f758ae0be205ed2
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-10-05 08:01:19 +0000

    Reverse A string with Reverse Method and For loop

--- Q5.js
+++ Q5.js
@@ -1,16 +1,24 @@
 //5.Write a function that return the reverse strings
 
-function revStr(inputString){
-    if(typeof inputString !== "string"){
-        throw new Error("Only number are allowed");
-    }
-    let result = "";
-    for(let i = inputString.length-1; i>=0; i--){
-        result = result + inputString[i];
-  
-    }
-    return result
+//Using reverse method
+function reverse1Str(inputString){
+    const inputStr = inputString.split('');
+    const newArray = inputStr.reverse();
+    const result = newArray.join("");
+    console.log(result)
 }
 
- console.log(revStr("Suyog muley"));
-console.log(revStr("Suyog muley form pune office"));
+reverse1Str("Suyog Muley");
+
+
+//Without reverse method
+
+function reverseTheInput(inputString){
+let result = ""
+    for(let i = inputString.length-1; i>=0; i-- ){
+      result = result +  inputString[i]
+    }
+   return result;
+}
+
+console.log(reverseTheInput("Suyog Muley"));
