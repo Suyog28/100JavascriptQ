0dd6938da60000a5538b36313c47e031e533f46a
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-10-05 16:25:31 +0000

    Refactor

--- Q9.js
+++ Q9.js
@@ -1,7 +1,7 @@
 //9.Write function that generate and prints a multplication table for a given number upto a specific range
 
 function multplicationTable(num, range){
-    for(let i = 2; i<=range; i++){
+    for(let i = 1; i<=range; i++){
         console.log("table of: ", num +" "+ i*num) ;
 
     }
