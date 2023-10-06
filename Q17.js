f03625901f2907b49a407ca420df72e479cbd3cd
Suyog Muley <53749026+Suyog28@users.noreply.github.com>
2023-10-06 11:35:36 +0000

    given number is prime or not different approch

--- Q17.js
+++ Q17.js
@@ -1,7 +1,7 @@
 //17.Write a function that tells if the given number is prime or not
 
 function primeChecked(number){
-     const result = true;
+     let result = true;
     for(let i=2; i<number; i++){
         if(number % i === 0){
             result= false;
@@ -12,4 +12,24 @@ function primeChecked(number){
     return result;
 }
 
-console.log(primeChecked(5))
+console.log(primeChecked(4))
+
+function diffrentMethodPrimeOrNot(number){
+    let result = true;
+    if(number % 2 === 0){
+     result = false;
+    }
+if(result === true){
+        for(let i=3; i<number; i= i+2){
+            if(number % i === 0){
+                result = false;
+                break;
+            }
+        }
+       
+    }
+    return result;
+}
+
+console.log(diffrentMethodPrimeOrNot(4))
+ 
